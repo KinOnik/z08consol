396c59500b584c0ab39ee99b8d12d80e495b0b58
KinOnik <84587417+KinOnik@users.noreply.github.com>
2021-12-13 15:29:43 +0300

    Add files via upload

---
+++ 1consol/1consol/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _1consol
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("\n\tВведите строку (все англ. слова заменятся на :\'...\' ): \n\t");
+            string str = Console.ReadLine();
+            string rex = "[a-zA-Z]+";
+            string str2 = Regex.Replace(str, rex, "...");
+            Console.Write($"\n\tРезультат: {str2}");
+        }
+    }
+}
